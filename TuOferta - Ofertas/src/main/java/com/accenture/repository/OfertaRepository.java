30f5eb37215985b4828c417e5727ba6946a744d3
Santiago Acevedo <santiago.acevedo@accenture.com>
2018-09-06 14:29:11 -0500

    cambios

---
+++ TuOferta - Ofertas/src/main/java/com/accenture/repository/OfertaRepository.java
@@ -0,0 +1,15 @@
+package com.accenture.repository;
+
+import java.util.List;
+
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+import com.accenture.model.RegistrarRequest;
+
+@EnableScan
+public interface OfertaRepository extends CrudRepository<RegistrarRequest, String> {
+	public List<RegistrarRequest> findByFechaFinal(String fechaFinal);
+	public List<RegistrarRequest> findByIdnegocio(String idnegocio);
+	public RegistrarRequest findById(String id);
+}
